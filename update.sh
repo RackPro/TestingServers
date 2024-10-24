b4ece73282e6efbc021243f46e46cc209883836a
Alexande <tarasov.sasha@list.ru>
2024-10-24 16:25:50 +0300

    test

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
 apt update -y
-apt install htop vim lm-sensors sysbench stress-ng -y
+apt install htop vim lm-sensors sysbench stress-ng netperf postgresql -y
+cp -f htoprc /root/.config/htop/
+sensors-detected
